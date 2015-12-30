8ba905b675681710fca8009d5cf19f836591a7f9
姜浩 <jh80281@alibaba-inc.com>
2015-12-30 23:37:16 +0800

    commit3

--- app/src/main/java/com/example/jianghao/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/jianghao/gitdemo/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Log.d("JH","3");
+
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
